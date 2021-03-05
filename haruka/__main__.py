4c3740309c201323dfa17068979158f1369373df
AnonymousKiller-hub <77574025+AnonymousKiller-hub@users.noreply.github.com>
2021-03-05 08:41:16 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -24,16 +24,16 @@ PM_START = """Hello {}, my name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm a group manager bot!
 
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
+MADE WITH ❤️ BY - @Anonymousishere
 
-MADE BY - @Anonymousishere
+SO WHAT ARE YUOU WAITING FOR ADD ME TO YOUR GROUP AND GIVE ALL PERMISSIONS😉
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
  Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+.
 """
 
 
